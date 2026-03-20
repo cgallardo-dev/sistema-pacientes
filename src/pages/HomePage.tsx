b14128c67b43e39e9830c94667111675080e9440
cgallardo-dev <carlosgallardo1810@gmail.com>
2026-03-20 11:48:14 -0500

    fix: quitar imports no usados

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Busqueda from './Busqueda';
+
+import { Link} from 'react-router-dom';
+
 function HomePage() {
 
     return (
